b00c8362f0acef3709ceef674287c3d3ffe028f4
Bhekumuzi Sanele Vilakazi <bvilakazi023a@student.wethinkcode.co.za>
2024-10-08 19:23:52 +0200

    Fixed The Failed To Configure a DataSource error

--- src/main/java/com/stores/Inventory/InventoryApplication.java
+++ src/main/java/com/stores/Inventory/InventoryApplication.java
@@ -2,10 +2,18 @@ package com.stores.Inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class InventoryApplication {
 
+	@GetMapping()
+	public String exampleString(){
+		return "This is for test perpose";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
